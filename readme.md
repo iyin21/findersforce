292b410875c6e6f792db9a2f160063281ace9c30
ibenge <benjamin.uforo@gmail.com>
2022-08-18 01:09:14 +0100

    chore: add appspec.yml for deployment

--- readme.md
+++ readme.md
@@ -2,8 +2,17 @@ finders force - a multi user HRM system built with Vite and documented with stor
 
 #### To install
 
-checkout of the master branch to <devname-branchName> and run `yarn install`
+checkout of master into <devname-branchName> and run `yarn install`
+
+
+### GIT
+
+Commits not inline with conventional commits would be rejected.
 
 ### Note
 
 In order to scale the application in the easiest and most maintainable way, keep most of the code inside the features folder, which should contain different feature-based things. Every feature folder should contain domain specific code for a given feature. This will allow you to keep functionalities scoped to a feature and not mix its declarations with shared things. This is much easier to maintain than a flat folder structure with many files. [bulletProofReact](https://github.com/alan2207/bulletproof-react/blob/master/docs/project-structure.md)
+
+
+# Note
+Write tests.
--- src/main.tsx
+++ src/main.tsx
@@ -1,23 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import * as Sentry from "@sentry/react";
-import { BrowserTracing } from "@sentry/tracing";
-import App from './App'
-import './global.scss'
+import React from "react"
+import ReactDOM from "react-dom/client"
+import * as Sentry from "@sentry/react"
+import { BrowserTracing } from "@sentry/tracing"
+import App from "./App"
+import "./global.scss"
 
 Sentry.init({
-  dsn: "https://af392b294aa4443d93f43b5e19d0b128@o1317059.ingest.sentry.io/6652714",
-  integrations: [new BrowserTracing()],
+    dsn: "https://af392b294aa4443d93f43b5e19d0b128@o1317059.ingest.sentry.io/6652714",
+    integrations: [new BrowserTracing()],
 
-  // Set tracesSampleRate to 1.0 to capture 100%
-  // of transactions for performance monitoring.
-  // We recommend adjusting this value in production
-  tracesSampleRate: 1.0,
-});
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 1.0,
+})
 
-
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 )
