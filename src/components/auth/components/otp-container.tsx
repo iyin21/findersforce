f75f589d67949317fddcbb6783f016fd0edb3698
Ganiyu Abiodun <timothyabiodun300@gmail.com>
2022-10-24 10:39:22 +0100

    chore: convert imput elements to component

--- src/components/auth/components/otp-container.tsx
+++ src/components/auth/components/otp-container.tsx
@@ -1,38 +1,22 @@
 import React from "react";
 
 const OtpContainer = () => {
+    const value = [1,2,3,4,5,6];
+
+    const inputContainer =  value.map(num => {
+        return (
+            <input 
+                type="text"
+                key={num}
+                placeholder={num.toString()}
+                className="w-full p-6 rounded-lg text-black border h-16"
+            />
+        );
+    });
+
     return (
         <div className="grid grid-cols-6 w-full mb-[50px] gap-x-4">
-            <input 
-                type="text"
-                placeholder="1"
-                className="w-full p-6 rounded-lg text-black border h-16"
-            />
-            <input 
-                type="text"
-                placeholder="2"
-                className="w-full p-6 rounded-lg text-black border h-16"
-            />
-            <input 
-                type="text"
-                placeholder="3"
-                className="w-full p-6 rounded-lg text-black border h-16"
-            />
-            <input 
-                type="text"
-                placeholder="4"
-                className="w-full p-6 rounded-lg text-black border h-16"
-            />
-            <input 
-                type="text"
-                placeholder="5"
-                className="w-full p-6 rounded-lg text-black border h-16"
-            />
-            <input 
-                type="text"
-                placeholder="6"
-                className="w-full p-6 rounded-lg text-black border h-16"
-            />
+            {inputContainer}
         </div>
     );
 }
