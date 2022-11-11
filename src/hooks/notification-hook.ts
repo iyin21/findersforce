26c6095085ad11598625b8eb0b2b0445deae0194
Ganiyu Abiodun <timothyabiodun300@gmail.com>
2022-11-11 11:44:54 +0100

    chore: move interface into types

--- src/hooks/notification-hook.ts
+++ src/hooks/notification-hook.ts
@@ -4,38 +4,9 @@ import { useQuery } from "@tanstack/react-query"
 import { AxiosError } from "axios"
 import { showNotification } from "@mantine/notifications"
 import useAxiosPrivate from "./../services/usePrivateAxios"
+import { NotificationResponse } from "types/notification-types"
 
-interface NotificationResponse {
-    count: number
-    status: boolean
-    pagination: {
-        next: {}
-        prev: {}
-    }
-    data: {
-        data: NotificationData[]
-    }
-}
 
-interface NotificationData {
-    _id: string
-    sender: string | any
-    description: string
-    receiver: {
-        _id: string
-        email: string
-        firstName: string
-        lastName: string
-        userName: string
-        phoneNumber: string
-    }
-    title: string
-    event: string
-    createdAt: Date | string
-    updatedAt: Date | string
-    __v: number
-    readStatus: boolean
-}
 
 export default function useUserNotification() {
     const { state } = useAuthContext()
---
+++ src/types/notification-types.ts
@@ -0,0 +1,31 @@
+export interface NotificationResponse {
+    count: number
+    status: boolean
+    pagination: {
+        next: {}
+        prev: {}
+    }
+    data: {
+        data: NotificationData[]
+    }
+}
+
+export interface NotificationData {
+    _id: string
+    sender: string | any
+    description: string
+    receiver: {
+        _id: string
+        email: string
+        firstName: string
+        lastName: string
+        userName: string
+        phoneNumber: string
+    }
+    title: string
+    event: string
+    createdAt: Date | string
+    updatedAt: Date | string
+    __v: number
+    readStatus: boolean
+}
