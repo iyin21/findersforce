e736771a07c5bd32081864547e123a89de7940e8
bethel enunwa <enunwabethel@gmail.com>
2022-11-16 17:51:18 +0100

    fix build issues

--- test/E2E/planner.spec.ts
+++ test/E2E/planner.spec.ts
@@ -4,7 +4,6 @@ const URL = "http:localhost:3000/planner"
 test ("a user can view planner  page", async ({page}) => {
                 await page.goto(URL);
                 // expect a title "to contain" a substring.
-                await expect(page.locator("text=Log In")).toBeVisible();
                 await page.fill('#email', "ent@pussport.com" );
                 await page.fill('#password', "Password22@");
 
