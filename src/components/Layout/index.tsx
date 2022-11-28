940acee10d54963b06946b1c212e3f4812cdf5d3
bethel enunwa <enunwabethel@gmail.com>
2022-11-28 21:09:22 +0100

    chore: refactor layout code

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -10,11 +10,11 @@ const Layout = ({ children, noTopNav }: LayoutProps) => {
     const [openSideBar, setOpenSideBar] = useState(false)
     return (
         <div className="h-screen relative">
-            <div className="fixed right-0 z-20 bg-white-100 lg:w-[84%] w-full">
+            <div className="fixed right-0 z-20 bg-white-100 lg:w-[83%] md:w-[73%] w-full">
                 <NavBar noTopNav={noTopNav} setOpenSideBar={setOpenSideBar}  />
             </div>
-            <div className="relative md:pl-64 h-full">
-                <div className="hidden md:block fixed bg-black-100 left-0 w-[16%]  h-[100%] overflow-y-auto">
+            <div className="relative md:pl-56 lg:pl-64 h-full">
+                <div className="hidden md:block fixed bg-black-100 left-0 lg:w-[17%] md:w-[27%] h-[100%] overflow-y-auto">
                     <Sidebar/>
                 </div>
                 <>
