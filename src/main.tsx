5eb62e032b1bd821c3867292c103589aae00d7e6
Ufonabasi Uduak umo <ufonumo@gmail.com>
2022-12-20 10:33:02 +0100

    fix: fixed ts error

--- src/main.tsx
+++ src/main.tsx
@@ -9,6 +9,8 @@ import { MantineProvider } from "@mantine/core"
 import { AuthProvider } from "./pages/auth/context/authContext"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { NotificationsProvider } from "@mantine/notifications"
+// eslint-disable-next-line no-unused-vars
+import { Buffer } from "buffer/"
 
 const queryClient = new QueryClient()
 
